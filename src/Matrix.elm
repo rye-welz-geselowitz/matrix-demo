5af6b179193fccb07b0c4218e68ec2453bc7899f
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2018-06-01 17:38:10 -0400

    write library & tests for matrix multiplication and transposition

---
+++ src/Matrix.elm
@@ -0,0 +1,234 @@
+module Matrix exposing (empty, fromList, multiply, toList, transpose)
+
+import Array exposing (Array)
+
+
+type Matrix a
+    = Matrix
+        { dimensions : ( Int, Int ) -- (m row x n cols)
+        , data : Array a
+        }
+
+
+
+--REPRESENTATION CONVERSIONS
+
+
+{-| Takes a list of lists. Each member list represents a row in the matrix.
+Returns either
+(a) Just a Matrix representation of the data or
+(b) Nothing, if the number of columns, in the list of rows is inconsistent
+-}
+fromList : List (List a) -> Maybe (Matrix a)
+fromList vals =
+    getNumCols vals
+        |> Maybe.map
+            (empty
+                |> setData (vals |> List.concatMap identity |> Array.fromList)
+                |> setRowNumber (List.length vals)
+                |> flip setColNumber
+            )
+
+
+{-| Produces a list representation of a matrix, with each member list
+representing a row in the matrix
+-}
+toList : Matrix a -> List (List a)
+toList matrix =
+    data matrix
+        |> Array.toList
+        |> List.indexedMap (,)
+        |> List.foldl (buildListFromMatrix matrix) []
+
+
+
+--MATRIX OPERATIONS
+
+
+{-| Multiplies two matrices a and b. If the dimensions allow for multiplcation, returns
+Just ab. Otherwise returns Nothing
+-}
+multiply : Matrix Float -> Matrix Float -> Maybe (Matrix Float)
+multiply a b =
+    let
+        ( m, n ) =
+            ( getM a, getN b )
+    in
+    if getN a == getM b && (m > 0) && (n > 0) then
+        Array.initialize (m * n) identity
+            |> Array.map (idxToIj ( m, n ) >> dotProductAtCoordinates a b)
+            |> matrix ( m, n )
+            |> Just
+    else
+        Nothing
+
+
+{-| Transposes a matrix
+-}
+transpose : Matrix a -> Matrix a
+transpose a =
+    let
+        ( m, n ) =
+            a |> dimensions |> flipTuple
+    in
+    Array.initialize (m * n) identity
+        |> Array.foldl (accumulateTransposedData a) Array.empty
+        |> matrix ( m, n )
+
+
+
+--HELPERS
+
+
+dotProductAtCoordinates : Matrix Float -> Matrix Float -> ( Int, Int ) -> Float
+dotProductAtCoordinates a b ( i, j ) =
+    List.range 0 (getN a - 1)
+        |> List.foldl
+            (\idx2 acc ->
+                case ( get a ( i, idx2 ), get b ( idx2, j ) ) of
+                    ( Just itemFromA, Just itemFromB ) ->
+                        acc + (itemFromA * itemFromB)
+
+                    _ ->
+                        acc
+            )
+            0.0
+
+
+accumulateTransposedData : Matrix a -> Int -> Array a -> Array a
+accumulateTransposedData a idx acc =
+    case idx |> idxToIj ( getN a, getM a ) |> flipTuple |> get a of
+        Nothing ->
+            acc
+
+        Just item ->
+            Array.push item acc
+
+
+buildListFromMatrix : Matrix a -> ( Int, a ) -> List (List a) -> List (List a)
+buildListFromMatrix matrix ( idx, val ) acc =
+    let
+        ( i, j ) =
+            idxToIj (dimensions matrix) idx
+    in
+    case List.reverse acc of
+        lastRow :: rest ->
+            if i >= List.length acc then
+                initiateRow acc val
+            else
+                List.append rest [ List.append lastRow [ val ] ]
+
+        _ ->
+            initiateRow acc val
+
+
+empty : Matrix a
+empty =
+    matrix ( 0, 0 ) Array.empty
+
+
+getIdx : ( Int, Int ) -> Matrix a -> Int
+getIdx ( i, j ) matrix =
+    (i * getN matrix) + j
+
+
+get : Matrix a -> ( Int, Int ) -> Maybe a
+get matrix ( i, j ) =
+    let
+        idx =
+            getIdx ( i, j ) matrix
+    in
+    matrix |> data |> Array.get idx
+
+
+idxToIj : ( Int, Int ) -> Int -> ( Int, Int )
+idxToIj ( _, colCount ) idx =
+    ( idx // colCount, rem idx colCount )
+
+
+flipTuple : ( a, a ) -> ( a, a )
+flipTuple ( a, b ) =
+    ( b, a )
+
+
+matrix : ( Int, Int ) -> Array a -> Matrix a
+matrix dimensions data =
+    Matrix
+        { dimensions = dimensions
+        , data = data
+        }
+
+
+setData : Array a -> Matrix a -> Matrix a
+setData data (Matrix matrix) =
+    Matrix { matrix | data = data }
+
+
+setRowNumber : Int -> Matrix a -> Matrix a
+setRowNumber m (Matrix matrix) =
+    Matrix { matrix | dimensions = ( m, Tuple.second matrix.dimensions ) }
+
+
+setColNumber : Int -> Matrix a -> Matrix a
+setColNumber n (Matrix matrix) =
+    Matrix { matrix | dimensions = ( Tuple.first matrix.dimensions, n ) }
+
+
+setDimensions : ( Int, Int ) -> Matrix a -> Matrix a
+setDimensions dimensions (Matrix matrix) =
+    Matrix { matrix | dimensions = dimensions }
+
+
+dimensions : Matrix a -> ( Int, Int )
+dimensions (Matrix { dimensions, data }) =
+    dimensions
+
+
+data : Matrix a -> Array a
+data (Matrix { dimensions, data }) =
+    data
+
+
+getN : Matrix a -> Int
+getN =
+    dimensions >> Tuple.second
+
+
+getM : Matrix a -> Int
+getM =
+    dimensions >> Tuple.first
+
+
+getNumCols : List (List a) -> Maybe Int
+getNumCols matrix =
+    let
+        n =
+            List.head matrix |> Maybe.withDefault [] |> List.length
+    in
+    if matrix |> validateList (validateNumColsForRow n) then
+        Just n
+    else
+        Nothing
+
+
+validateList : (a -> Bool) -> List a -> Bool
+validateList f list =
+    list |> List.foldl (accumulateValidation f) True
+
+
+accumulateValidation : (a -> Bool) -> a -> Bool -> Bool
+accumulateValidation f item acc =
+    if not acc then
+        acc
+    else
+        f item
+
+
+validateNumColsForRow : Int -> List a -> Bool
+validateNumColsForRow n row =
+    List.length row == n
+
+
+initiateRow : List (List a) -> a -> List (List a)
+initiateRow list item =
+    List.append list [ [ item ] ]
---
+++ tests/Tests.elm
@@ -0,0 +1,153 @@
+module Tests exposing (..)
+
+import Expect
+import Matrix
+import Test exposing (..)
+
+
+all : Test
+all =
+    describe "Matrix tests"
+        [ describe "Transposing a matrix"
+            [ test "transposes an n x n matrix" <|
+                \_ ->
+                    let
+                        a =
+                            Matrix.fromList
+                                [ [ 1, 2, 3 ]
+                                , [ 4, 5, 6 ]
+                                , [ 7, 8, 9 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        transposed =
+                            [ [ 1, 4, 7 ], [ 2, 5, 8 ], [ 3, 6, 9 ] ]
+                    in
+                    Expect.equal transposed (Matrix.transpose a |> Matrix.toList)
+            , test "transposes an n x n+1 matrix" <|
+                \_ ->
+                    let
+                        a =
+                            Matrix.fromList
+                                [ [ 1, 2, 3, 0 ]
+                                , [ 4, 5, 6, 10 ]
+                                , [ 7, 8, 9, 100 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        transposed =
+                            [ [ 1, 4, 7 ], [ 2, 5, 8 ], [ 3, 6, 9 ], [ 0, 10, 100 ] ]
+                    in
+                    Expect.equal transposed (Matrix.transpose a |> Matrix.toList)
+            ]
+        , describe "Multiplication"
+            [ test "Multiplying empty matrices yields Nothing" <|
+                \_ ->
+                    let
+                        actual =
+                            Matrix.multiply Matrix.empty Matrix.empty
+
+                        expected =
+                            Nothing
+                    in
+                    Expect.equal expected (actual |> Maybe.map Matrix.toList)
+            , test "Multiplying two n x n matrix yields Just n x n matrix" <|
+                \_ ->
+                    let
+                        a =
+                            Matrix.fromList
+                                [ [ 1, 2, 3 ]
+                                , [ 0, 1, 0 ]
+                                , [ 2, 2, 3 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        b =
+                            Matrix.fromList
+                                [ [ 1, 3, 0 ]
+                                , [ 2, 2, 1 ]
+                                , [ 0, 0, 3 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        actual =
+                            Matrix.multiply a b
+
+                        expected =
+                            [ [ 5, 7, 11 ], [ 2, 2, 1 ], [ 6, 10, 11 ] ]
+                    in
+                    Expect.equal (Just expected) (actual |> Maybe.map Matrix.toList)
+            , test "Multiply m x n and n x m yields Just n x n matrix" <|
+                \_ ->
+                    let
+                        a =
+                            Matrix.fromList
+                                [ [ 1, 10, 1, 1 ]
+                                , [ 5, 1, 5, 1 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        b =
+                            Matrix.fromList
+                                [ [ 8, 9 ]
+                                , [ 1, 2 ]
+                                , [ 3, 4 ]
+                                , [ 0, 10 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        actual =
+                            Matrix.multiply a b
+
+                        expected =
+                            [ [ 21, 43 ], [ 56, 77 ] ]
+                    in
+                    Expect.equal (Just expected) (actual |> Maybe.map Matrix.toList)
+            , test "Multiplying m x n matrix by n+1 x m matrix yeilds Nothing" <|
+                \_ ->
+                    let
+                        a =
+                            Matrix.fromList
+                                [ [ 1, 10, 1, 1 ]
+                                , [ 5, 1, 5, 1 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        b =
+                            Matrix.fromList
+                                [ [ 8, 9 ]
+                                , [ 1, 2 ]
+                                , [ 3, 4 ]
+                                , [ 0, 10 ]
+                                , [ 0, 0 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+                    in
+                    Expect.equal Nothing (Matrix.multiply a b)
+            , test "Multiplying m x n matrix by n x m+1 matrix yields Just m x m+1 matrix" <|
+                \_ ->
+                    let
+                        a =
+                            Matrix.fromList
+                                [ [ 1, 10, 1, 1 ]
+                                , [ 5, 1, 5, 1 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        b =
+                            Matrix.fromList
+                                [ [ 8, 9, 0 ]
+                                , [ 1, 2, 1 ]
+                                , [ 3, 4, 0 ]
+                                , [ 0, 10, 1 ]
+                                ]
+                                |> Maybe.withDefault Matrix.empty
+
+                        expected =
+                            [ [ 21, 43, 11 ]
+                            , [ 56, 77, 2 ]
+                            ]
+                    in
+                    Expect.equal (Just expected) (Matrix.multiply a b |> Maybe.map Matrix.toList)
+            ]
+        ]
